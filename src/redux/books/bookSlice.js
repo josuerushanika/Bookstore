73dc42678f5fe6b361c7e98ea742cc7c7e808026
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-14 19:45:27 +0200

    Adding book object

---
+++ src/redux/books/bookSlice.js
@@ -0,0 +1,24 @@
+const bookList = [
+  {
+    item_id: 'item1',
+    title: 'The Magician',
+    author: 'Alain Quote',
+    category: 'Roman'
+  },
+
+  {
+    item_id: 'item2',
+    title: 'Second War',
+    author: 'Aime Loqw',
+    category: 'History',
+  },
+
+  {
+    item_id: 'item3',
+    title: 'Network Principile',
+    author: 'Josue Rushanika',
+    category: 'Technology',
+  },
+];
+
+export default bookList;
