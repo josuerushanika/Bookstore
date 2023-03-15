f307d74bd0ec8c33c513a9457c87b9bdc91e9db6
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-15 21:17:19 +0200

    Add some style

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/bookSlice';
+import '../styles/Form.css';
 
 const Form = () => {
   const dispatch = useDispatch();
@@ -15,21 +16,29 @@ const Form = () => {
   };
 
   return (
-    <form onSubmit={handleAddBook}>
-      <h3>Book Title:</h3>
-      <input
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        type="text"
-        placeholder="title"
-      />
-      <br />
-      Author:
-      <input type="text" placeholder="Author" />
-      <br />
-      <button type="submit">Add</button>
-    </form>
-
+    <div className="form-container">
+      <form onSubmit={handleAddBook}>
+        <h3>Book Title:</h3>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="title"
+          required
+        />
+        <br />
+        Author:
+        <input
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          type="text"
+          placeholder="Author"
+          required
+        />
+        <br />
+        <button type="submit">Add</button>
+      </form>
+    </div>
   );
 };
 
