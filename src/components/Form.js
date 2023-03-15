b77a8c91e6e2612d26fe5eb8dfdcc6096680e3c3
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-15 20:24:24 +0200

    Update some functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/bookSlice';
 
-const Form = () => (
-  <>
+const Form = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleAddBook = (e) => {
+    e.preventDefault();
+    dispatch(addBook({ title, author }));
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
     <form>
       Book Title:
       <input type="text" placeholder="title" />
@@ -11,8 +24,8 @@ const Form = () => (
       <br />
       <button type="submit">Add</button>
     </form>
-  </>
 
-);
+  );
+};
 
 export default Form;
