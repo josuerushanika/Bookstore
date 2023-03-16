d72e991bb9bffe12e0071f1d25d9f512b7e1fd18
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-17 01:14:09 +0200

    Update some functions

--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -7,14 +7,34 @@ const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstor
 export const postBook = createAsyncThunk('books/postBook',
   async (bookData, thunkApi) => {
     try {
-      const resData = await axios.post(`${url}`, bookData);
+      const resData = await axios.post(`${url}/books`, bookData);
       return resData.data;
     } catch (error) {
       return thunkApi.rejectWithValue(error?.data?.message || 'something went wrong');
     }
   });
 
-  
+export const getBooks = createAsyncThunk('books/getBook',
+  async (__, thunkApi) => {
+    try {
+      const res = await axios(`${url}/books`);
+      return res.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error?.data?.message || 'something went wrong');
+    }
+  });
+
+export const deleteBook = createAsyncThunk('books/deleteBook', async (id, thunkApi) => {
+  try {
+    const res = await axios.delete(`${url}/books/${id}`);
+    return res.data;
+  } catch (error) {
+    return thunkApi.rejectWithValue(
+      error?.data?.message || 'Something went wrong',
+    );
+  }
+});
+
 const initialState = {
   bookList,
 };
@@ -42,7 +62,36 @@ const bookSlice = createSlice({
       };
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(postBook.pending, (state) => {
+      // eslint-disable-next-line no-param-reassign
+      state.isLoading = true;
+    });
+    .addCase(postBook.fulfilled, (state) => {
+      state.isLoading = false;
+    })
+    .addCase(postBook.rejected, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getBooks.pending, (state) => {
+      state.isLoading = true;
+    })
+      .addCase(getBooks.fulfilled, (state, action) =>{
+        state.isLoading = false;
+        const resData = action.payload;
+        const newBookArray = Object.keys(resData).map((id) =>{
+          state.isLoading = false;
+          const resData = action.payload;
+          const newBookArray = Object.keys(resData).map(id) =>
+        })
+        state.books = newBookArray;
+      })
+      .addCase(getBooks.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
 });
 
 export const { addBook, removeBook } = bookSlice.actions;
+export const booksActions = bookSlice.actions;
 export default bookSlice.reducer;
