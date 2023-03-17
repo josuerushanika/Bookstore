c7e44cdc278c811cf1f2c8d91bae987a7f549630
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-17 21:24:20 +0200

    Adding first style

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import { addBook, postBook } from '../redux/books/bookSlice';
-import '../styles/Form.css';
 
 const Form = () => {
   const dispatch = useDispatch();
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
+import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => (
 
   <header>
-    <h1>BOOK STORE</h1>
-    <nav>
-      <NavLink to="/">Books</NavLink>
-      <NavLink to="/categories">Categories</NavLink>
-    </nav>
+    <div className={styles.header}>
+      <h1>BookStore  </h1>
+      <nav>
+        <NavLink to="/">Books</NavLink>
+        <NavLink to="/categories">Categories</NavLink>
+      </nav>
+    </div>
+
+    <button type="button">
+      <FaUser />
+    </button>
   </header>
 
 );
